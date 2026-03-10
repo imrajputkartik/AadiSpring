c5ca5ae10a53708d8ee3a601a6c95f7ef6a53fbf
imrajputkartik <kartikchauhan647@gmail.com>
2026-03-10 12:22:09 +0530

    update

--- src/main/java/com/bhanu/spring/model/Student.java
+++ src/main/java/com/bhanu/spring/model/Student.java
@@ -1,10 +1,18 @@
 package com.bhanu.spring.model;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Student {
 
 	private String name, gender;
 	private int id;
 	private Address address;
+
+	List<Integer> list;
+	Set<Integer> set;
+	Map<Integer, String> map;
 
 	public Student() {
 		super();
@@ -12,12 +20,40 @@ public class Student {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Student(String name, String gender, int id, Address address) {
+	public Student(String name, String gender, int id, Address address, List<Integer> list, Set<Integer> set,
+			Map<Integer, String> map) {
 		super();
 		this.name = name;
 		this.gender = gender;
 		this.id = id;
 		this.address = address;
+		this.list = list;
+		this.set = set;
+		this.map = map;
+	}
+
+	public List<Integer> getList() {
+		return list;
+	}
+
+	public void setList(List<Integer> list) {
+		this.list = list;
+	}
+
+	public Set<Integer> getSet() {
+		return set;
+	}
+
+	public void setSet(Set<Integer> set) {
+		this.set = set;
+	}
+
+	public Map<Integer, String> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<Integer, String> map) {
+		this.map = map;
 	}
 
 	public Address getAddress() {
@@ -58,7 +94,8 @@ public class Student {
 
 	@Override
 	public String toString() {
-		return "Student [name=" + name + ", gender=" + gender + ", id=" + id + ", address=" + address + "]";
+		return "Student [name=" + name + ", gender=" + gender + ", id=" + id + ", address=" + address + ", list=" + list
+				+ ", set=" + set + ", map=" + map + "]";
 	}
 
 }
